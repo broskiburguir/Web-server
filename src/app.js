e61102447146b3ca510f2a7ab0e782eb7db1b254
bourguir <bourguignon.lucas01@gmail.com>
2022-06-11 15:59:38 +0200

    Beginning dinamic template

--- src/app.js
+++ src/app.js
@@ -11,9 +11,9 @@ console.log(path.join(__dirname, '../public'))
 const app = express()
 const publicDirectory = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectory, { extensions: ['html'] }))
 
-// create html page for about
 
 app.get('', (req, res) => {
   res.send('<h1>Weather<h1>')
@@ -37,9 +37,6 @@ app.get('/weather', (req, res) => {
     location: 'Zimbabwe'
   })
 })
-
-// app.com/help
-// app.com/about
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
